6343e45c65bbadef49173c93a50cee7b5deff476
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-02 16:48:36 +0530

    Setting the code for rendering meals data and images

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,26 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/assets/components/meals/meals-grid";
 
 export default function MealsPage() {
-    return (
-        <main>
-            <h1>Meals Page</h1>
-            <p><Link href='/community'>To Community</Link></p>
-        </main>
-    )
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favorite recipes and cook it yourself. It is easy and
+          fun!!!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your fav recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
