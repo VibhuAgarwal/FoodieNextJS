9b19f00c4ce0d72ee005cb477fc9cbe57aefc898
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-07 14:35:36 +0530

    Adding Error and not-found page when undefined route is requested.

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFOundPage() {
+    return (
+        <main className="not-found">
+            <h1>Not Found</h1>
+            <p>Unfortunately, we cannot find the requested page</p>
+        </main>
+    )
+}
