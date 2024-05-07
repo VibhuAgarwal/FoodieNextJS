f11fd9820883538c5a28ad5a6e3af866e1c57742
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-07 16:03:13 +0530

    Adding not found for every wrong route for meal

---
+++ app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFOundPage() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, we cannot find the requested meal data :( </p>
+    </main>
+  );
+}
