8e5094c4f1dd0ddac1bcc4fd99d772ee2a779459
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-05-08 18:29:18 +0530

    Handle form submission with help of NextJS server actions

--- assets/components/meals/image-picker.jsx
+++ assets/components/meals/image-picker.jsx
@@ -1,17 +1,38 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState()
     const imageInput = useRef();
+
     function handlePickClick() {
         imageInput.current.click();
     }
 
+    function handleImageChange(e) {
+        e.preventDefault();
+        const file = e.target.files[0];
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        }
+        fileReader.readAsDataURL(file);
+    }
+
     return <div className={classes.picker}>
-        <label htmlFor={name}>{label}</label >
+        <label htmlFor={name}>{label}</label>
         <div className={classes.controls}>
+            <div className={classes.preview}>
+                {!pickedImage && <p>No Image Picked yet</p>}
+                {pickedImage && <Image src={pickedImage} alt='Picked Image' fill />}
+            </div>
             <input
                 className={classes.input}
                 type="file"
@@ -19,6 +40,8 @@ export default function ImagePicker({ label, name }) {
                 accept='image/png, image/jpg, image/jpeg'
                 name={name}
                 ref={imageInput}
+                onChange={handleImageChange}
+                required
             />
             <button className={classes.button} type='button' onClick={handlePickClick}>
                 Pick an Image
